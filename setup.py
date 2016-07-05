eb9d1cdbf1d3d08d273beebf551efb1e43765ef6
Chris Dutra <cdutra@apprenda.com>
2016-07-05 15:24:13 -0400

    updating version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name="redicrypt",
-    version="0.0.5",
+    version="0.0.6",
     author="Chris Dutra",
     author_email="cdutra@apprenda.com",
     description="Python-based cryptography package for redis.",
