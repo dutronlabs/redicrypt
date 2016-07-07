215f0b5cf3244ff97f65359ab69492af517e1b95
Chris Dutra <cdutra@apprenda.com>
2016-07-07 18:13:10 -0400

    fixed some major bugs found in previous release

--- redicrypt/redicrypt.py
+++ redicrypt/redicrypt.py
@@ -10,13 +10,18 @@ DEFAULT_KEY_PATH = './key'
 
 # utility function used across all methods here to make sure defaults are read if no path is specified
 def get_paths(key, ivr):
-    _key = DEFAULT_KEY_PATH if key is None else key
-    _ivr = DEFAULT_IV_PATH if ivr is None else ivr
+    # revert to defaults in case environment vars get messed up
+    _key = DEFAULT_KEY_PATH if os.environ.get('CURRENT_KEY_PATH') is None else os.environ['CURRENT_KEY_PATH']
+    _ivr = DEFAULT_IV_PATH if os.environ.get('CURRENT_IV_PATH') is None else os.environ['CURRENT_IV_PATH']
     return _key, _ivr
 
 
 def initialize_encryption(key_path=None, ivr_path=None):
     """ This function (re)generates the keys we'll use for encryption and stores them on the file system."""
+    if key_path is not None:
+        os.environ['CURRENT_KEY_PATH'] = key_path
+    if ivr_path is not None:
+        os.environ['CURRENT_IV_PATH'] = ivr_path
     key_path, ivr_path = get_paths(key_path, ivr_path)
     # We are using w+ here because during initialization, we are absolutely going to want to truncate it
     # and write a new key.
@@ -46,14 +51,14 @@ def set(name, value, key_path=None, ivr_path=None, overredis=None):
     r.set(name, ciphertext)
 
 
-def get(name, key_path=None, ivr_path=None, overredis=None):
+def get(name, key_path=None, ivr_path=None, overredis=None, redis_conn=None):
     """ This gets a value that is encrypted in redis."""
     try:
         key_path, ivr_path = get_paths(key_path, ivr_path)
-        r = overredis if overredis is not None else loadconfiguration()
+        r = overredis if overredis is not None else loadconfiguration(host=redis_conn['host'], password=redis_conn['password'])
         cipher = r.get(name)
         if cipher is None:
-            return ValueError("No value exists for given key.")
+            return None
         aes = get_hash(key_path, ivr_path)
         return aes.decrypt(cipher)
     except TypeError, e:
@@ -78,19 +83,20 @@ def hget(name, field, key_path=None, ivr_path=None, overredis=None):
     return aes.decrypt(encrypted)
 
 
-def test_availability(host=None):
-    test = loadconfiguration(host=host)
+def test_availability(host=None, password=None):
+    test = loadconfiguration(host=host, password=password)
     return True if test is not None else False
 
 
-def loadconfiguration(host=None, password=None):
+def loadconfiguration(host=None, port=None, password=None):
     try:
         if host is None:
             host = os.environ['REDIS_ENDPOINT']
-        if password is None and os.environ.get['REDIS_PW'] is None:
+        _port = int(os.environ['REDIS_PORT']) if port is None else int(port)
+        if password is None and os.environ.get('REDIS_PW') is None:
             # check to see if default pw is loaded, but don't throw if None
-            return redis.StrictRedis(host=host)
-        password = password or os.environ.get['REDIS_PW']
-        return redis.StrictRedis(host=host, password=password)
+            return redis.StrictRedis(host=host, port=_port)
+        password = password or os.environ.get('REDIS_PW')
+        return redis.StrictRedis(host=host, password=password, port=_port)
     except Exception, e:
         raise e
